c06b4e6a9712c813b5ea205dde97fca71ddb0dee
lucianopereira86 <lusope86@gmail.com>
2021-08-08 09:37:00 -0300

    Teste unitário de integração concluído.

---
+++ src/RetornaTaxaDeJuros/RetornaTaxaDeJuros.Tests.TDD/IntegrationTests/MyWebFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using RetornaTaxaDeJuros.Presentation.API;
+
+namespace RetornaTaxaDeJuros.Tests.TDD.IntegrationTests
+{
+    public class MyWebFactory : WebApplicationFactory<Startup>
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder().ConfigureWebHost((builder) =>
+            {
+                builder.UseStartup<TestStartup>();
+            });
+        }
+    }
+}
---
+++ src/RetornaTaxaDeJuros/RetornaTaxaDeJuros.Tests.TDD/IntegrationTests/TaxaJurosIntegrationTest.cs
@@ -0,0 +1,26 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RetornaTaxaDeJuros.Tests.TDD.IntegrationTests
+{
+    public class TaxaJurosIntegrationTest : IClassFixture<MyWebFactory>
+    {
+        private readonly HttpClient _client;
+
+        public TaxaJurosIntegrationTest(MyWebFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetTaxaJuros()
+        {
+            var httpResponse = await _client.GetAsync("/api/taxaJuros");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+        }
+    }
+}
