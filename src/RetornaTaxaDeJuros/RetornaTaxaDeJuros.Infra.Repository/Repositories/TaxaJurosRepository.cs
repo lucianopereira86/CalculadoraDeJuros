b9686a90e68e0ec3772caca347a363cc0f92cb7a
lucianopereira86 <lusope86@gmail.com>
2021-08-08 07:51:37 -0300

    criaçao do GetTaxaJuros no TaxaJurosRepository

---
+++ src/RetornaTaxaDeJuros/RetornaTaxaDeJuros.Infra.Repository/Repositories/TaxaJurosRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetornaTaxaDeJuros.Infra.Repository.Repositories
+{
+    public class TaxaJurosRepository
+    {
+        public double GetTaxaJuros()
+        {
+            return 0.01;
+        }
+    }
+}
