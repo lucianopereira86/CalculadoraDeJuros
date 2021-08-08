767602da1c0af49129b31e8e1e3f3f601e1fdb02
lucianopereira86 <lusope86@gmail.com>
2021-08-08 10:26:12 -0300

    TDD do calculo de juros - resultado mais simples.

---
+++ src/CalculadoraDeJuros/CalculadoraDeJuros.Tests.TDD/RepositoryTests/CalculaJurosRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace CalculadoraDeJuros.Tests.RepositoryTests
+{
+    public class CalculaJurosRepositoryTests
+    {
+        [Theory]
+        [InlineData(100.0, 5, 105.10)]
+        public void ShouldReturnSuccessWhenEqualsExpected(double valorInicial, int meses, double expected)
+        {
+            #region Arrange
+            double juros = 0.01;
+            #endregion Arrange
+
+            #region Act
+            double valorFinal = Math.Truncate((valorInicial * Math.Pow(1 + juros, meses)) * 100) / 100;
+            #endregion Act
+
+            #region Assert
+            Assert.Equal(valorFinal, expected);
+            #endregion Assert
+        }
+    }
+}
